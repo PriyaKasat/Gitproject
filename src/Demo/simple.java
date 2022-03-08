b66b38caf4a0710c235fa4b2478eb6ad593a7b96
laptop <priyankakasat1995@gmail.com>
2022-03-08 19:08:06 +0530

    This is modofied commit

---
+++ src/Demo/simple.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Simple program");
+		System.out.println("first program");
+	}
+
+}
